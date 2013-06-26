fb2d8e07fc06d5bd97a135db3318b97dc978cf0b
Kate Liu <kateycliu@gmail.com>
2013-06-25 20:49:33 -0700

    refine the layout

--- client/voting.js
+++ client/voting.js
@@ -14,6 +14,10 @@ Template.content.showHome = function () {
     return Session.equals("page", "home");
 }
 
+Template.content.showIntro = function () {
+    return Session.equals("page", "intro");
+}
+
 Template.content.showVote = function () {
     return Session.equals("page", "vote");
 }
@@ -22,9 +26,6 @@ Template.content.showReview = function () {
     return Session.equals("page", "review");
 }
 
-Template.content.showHelp = function () {
-    return Session.equals("page", "help");
-}
 
 Template.navbar.events({
     'click li' : function (e) {
@@ -35,6 +36,10 @@ Template.navbar.events({
         console.log("home");
         Session.set("page", "home");
     },
+    'click #intro' : function (e) {
+        console.log("intro");
+        Session.set("page", "intro");
+    },
     'click #vote' : function (e) {
         console.log("vote");
         Session.set("page", "vote");
@@ -43,16 +48,16 @@ Template.navbar.events({
     'click #review' : function (e) {
         console.log("review");
         Session.set("page", "review");
-    },
-    'click #help' : function (e) {
-        console.log("help");
-        Session.set("page", "help");
+    }
+});
+
+Template.vote.events({
+    'click .active' : function (e) {
+        console.log("active item");
     }
 });
 
 Template.vote.candidates = function () {
-    console.log("in vote.dandidates");
-    console.log(Candidates.find().count());
     return Candidates.find();
 };
 
@@ -66,21 +71,48 @@ Template.vote.isActive = function () {
 
 Template.container.rendered = function (e) {
     $('body').on('keypress', function (e) {
-        console.log(e.keyCode);
         var code = e.keyCode || e.which;
-        if (104 == code) {
-            // left
-            $('#left').click();
-        }
-        else if (108 == code) {
-            // right
-            $('#right').click();
-        }
-        else if (107 == code) {
-            // down
-        }
-        else if (106 == code) {
-            // up
+        var page = Session.get("page");
+        console.log(page);
+        console.log(e.keyCode);
+        switch (code) {
+            case 72:
+            case 104:
+                // H or h: left
+                $('#left').click();
+                break;
+            case 76:
+            case 108:
+                // L or l" right
+                $('#right').click();
+                break;
+            case 75:
+            case 107:
+                // K or k: down
+                $('.active').click();;
+                break;
+            case 65:
+            case 97:
+                // A or a: tab
+                if (page === "home") {
+                    console.log("going to click on intro");
+                    $('#intro').click();
+                }
+                else if (page === "intro") {
+                    console.log("going to click on vote");
+                    $('#vote').click();
+                }
+                else if (page === "vote") {
+                    console.log("going to click on review");
+                    $('#review').click();
+                }
+                else if (page === "review") {
+                    console.log("going to click on home");
+                    $('#home').click();
+                }
+                break;
+            default:
+                break;
         }
     });
 };
