7518dca7f8e84b3d4318906ae0320b186612691e
Kate Liu <kateycliu@gmail.com>
2013-07-01 02:13:52 -0700

    disable carousel auto-slide. fix hot key a

--- client/voting.js
+++ client/voting.js
@@ -5,11 +5,6 @@ Deps.autorun(function () {
 });
 
 Session.setDefault("page", "home");
-Session.setDefault("firstItem", true);
-
-$(document).ready(function () {
-    $('#myCarousel').carousel({ interval: false });
-});
 
 Template.content.showHome = function () {
     return Session.equals("page", "home");
@@ -26,7 +21,6 @@ Template.content.showVote = function () {
 Template.content.showReview = function () {
     return Session.equals("page", "review");
 }
-
 
 Template.navbar.events({
     'click li a' : function (e) {
@@ -51,6 +45,33 @@ Template.navbar.events({
     }
 });
 
+Template.navbar.rendered = function (e) {
+    $('body').on('keypress', function (e) {
+        var code = e.keyCode || e.which;
+        var page = Session.get("page");
+        console.log(page);
+        console.log(e.keyCode);
+        if (code === 97) {
+            // A or a: tab
+            if (page === "home") {
+                console.log("going to click on intro");
+                $('#intro').click();
+            }
+            else if (page === "intro") {
+                console.log("going to click on vote");
+                $('#vote').click();
+            }
+            else if (page === "vote") {
+                console.log("going to click on review");
+                $('#review').click();
+            }
+            else if (page === "review") {
+                console.log("showing a confirm box");
+            }
+        }
+    });
+};
+
 Template.vote.events({
     'click .active' : function (e) {
         console.log("active item");
@@ -62,6 +83,7 @@ Template.candidateInfo.candidates = function () {
 };
 
 Template.candidateInfo.rendered = function () {
+    $('#myCarousel').carousel({ interval: false });
     $("#candidate").first().addClass("active");
     $('body').on('keypress', function (e) {
         var code = e.keyCode || e.which;
@@ -90,53 +112,4 @@ Template.candidateInfo.rendered = function () {
         }
     });
 };
-
-
-//Template.container.rendered = function (e) {
-//    $('body').on('keypress', function (e) {
-//        var code = e.keyCode || e.which;
-//        var page = Session.get("page");
-//        console.log(page);
-//        console.log(e.keyCode);
-//        switch (code) {
-//            case 72:
-//            case 104:
-//                // H or h: left
-//                $('#left').click();
-//                break;
-//            case 76:
-//            case 108:
-//                // L or l" right
-//                $('#right').click();
-//                break;
-//            case 75:
-//            case 107:
-//                // K or k: down
-//                $('.active').toggleClass("selected");
-//                break;
-//            case 65:
-//            case 97:
-//                // A or a: tab
-//                if (page === "home") {
-//                    console.log("going to click on intro");
-//                    $('#intro').click();
-//                }
-//                else if (page === "intro") {
-//                    console.log("going to click on vote");
-//                    $('#vote').click();
-//                }
-//                else if (page === "vote") {
-//                    console.log("going to click on review");
-//                    $('#review').click();
-//                }
-//                else if (page === "review") {
-//                    console.log("going to click on home");
-//                    $('#home').click();
-//                }
-//                break;
-//            default:
-//                break;
-//        }
-//    });
-//};
 
